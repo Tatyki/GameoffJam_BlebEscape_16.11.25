7ff364a7baed7fa87cffe4ac963ad4fa963b7cdd
Gladkkiy <supermaxs2@mail.ru>
2025-11-21 01:51:15 +0300

    MyhaAndBleb
    123

---
+++ Assets/Scripts/SpiderWeb/ChargedWeb.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargedWeb : MonoBehaviour
+{
+    public RopeVerlet rope;
+    public bool isCharged;
+    private bool wasChargedLastFrame = false;
+    private LineRenderer lineRenderer;
+    private DragAndDrop ownerSpider; 
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        rope = GetComponent<RopeVerlet>();
+
+        FindOwnerSpider();
+    }
+
+    private void FindOwnerSpider()
+    {
+        DragAndDrop[] allSpiders = FindObjectsOfType<DragAndDrop>();
+        foreach (DragAndDrop spider in allSpiders)
+        {
+            if (spider.activeRope == this.rope)
+            {
+                ownerSpider = spider;
+                break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        bool previousChargeState = isCharged;
+        isCharged = CheckChargeStatus();
+
+        if (ownerSpider == null)
+        {
+            FindOwnerSpider(); 
+        }
+
+        if (ownerSpider != null)
+        {
+            ownerSpider.canMove = !isCharged;
+        }
+
+        if (isCharged)
+        {
+            lineRenderer.startColor = Color.blue;
+            lineRenderer.endColor = Color.blue;
+        }
+        else
+        {
+            lineRenderer.startColor = Color.white;
+            lineRenderer.endColor = Color.white;
+        }
+
+        if (wasChargedLastFrame && !isCharged)
+        {
+            UpdateConnectedWebs();
+        }
+
+        wasChargedLastFrame = isCharged;
+    }
+
+    private bool CheckChargeStatus()
+    {
+        if (rope.ropeEndPoint == null || !rope.endAttached)
+            return false;
+
+        DrawWeb endDrawWeb = rope.ropeEndPoint.gameObject.GetComponent<DrawWeb>();
+        if (endDrawWeb != null && endDrawWeb.ObjectIsCharged)
+            return true;
+
+        ChargedWeb connectedWeb = rope.ropeEndPoint.gameObject.GetComponent<ChargedWeb>();
+        if (connectedWeb != null && connectedWeb.isCharged)
+            return true;
+
+        return false;
+    }
+
+    private void UpdateConnectedWebs()
+    {
+        foreach (ChargedWeb web in FindObjectsOfType<ChargedWeb>())
+        {
+            if (web != this && web.rope.ropeEndPoint != null &&
+                web.rope.ropeEndPoint.gameObject == this.gameObject)
+            {
+                web.isCharged = web.CheckChargeStatus();
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (isCharged && collision.gameObject.CompareTag("bleb") || isCharged && collision.gameObject.CompareTag("Myha"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/myha.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using Pathfinding;
+using JetBrains.Annotations;
+
+public class myha : MonoBehaviour
+{
+   [Header("Settings")]
+    public float moveSpeed = 5f; 
+    public float reachDistance = 0.2f;
+    [Space]
+    private Transform[] pathPoints;
+    public Transform _path;
+    
+    private int currentPoint = 0;
+    private Rigidbody2D rb;
+    private AIPath path;
+
+    [Header("Detection")]
+    public float detectionRadius = 2f; 
+    public float checkTime = 2.5f; 
+    [Space]
+    public LayerMask hitMask;
+    public Transform lastPosition;  
+    bool spotted = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        path = GetComponent<AIPath>();
+
+        _path = GameObject.FindGameObjectWithTag("point").transform;
+
+        if (_path != null)
+        {
+            int childCount = _path.childCount;
+            pathPoints = new Transform[childCount];
+
+            for (int i = 0; i < childCount; i++)
+            {
+                pathPoints[i] = _path.GetChild(i);
+            }
+        }
+
+        lastPosition = GameObject.FindGameObjectWithTag("lastPosition").transform;
+
+        path.maxSpeed = moveSpeed;
+        path.canMove = false;
+    }
+    void Update()
+    {
+        blebFollow();
+
+        if(!spotted && currentPoint < pathPoints.Length && pathPoints.Length > 0)
+        {
+            moving(pathPoints[currentPoint]);
+            if(Vector2.Distance(transform.position, pathPoints[currentPoint].position) < reachDistance) currentPoint++;
+        }
+    }
+    void blebFollow()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius);
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("bleb"))
+            {
+                Vector2 direction = (hit.transform.position - transform.position);
+                RaycastHit2D _hit = Physics2D.Raycast(transform.position, direction, detectionRadius, hitMask);
+                Debug.DrawRay(transform.position, direction * detectionRadius, Color.red);
+
+                if(_hit.collider.CompareTag("bleb"))
+                {
+                    lastPosition.position = _hit.point;
+                    CancelInvoke("unspotted");
+                    spotted = true;
+                }
+            }
+        }
+        if(Vector2.Distance(transform.position, lastPosition.position) < reachDistance)
+        {
+            Invoke("unspotted",checkTime);
+        }
+        if (spotted) moving(lastPosition);
+    }
+    void unspotted()
+    {
+        spotted = false;
+        closestPoint();
+    }
+    void moving(Transform target)
+    {
+        path.canMove = true;
+        path.destination = target.position;
+    }
+    void closestPoint()
+    {
+        float minDistance = Vector2.Distance(transform.position, pathPoints[currentPoint].position);
+        for (int i = 1; i < pathPoints.Length; i++)
+        {
+            float dist = Vector2.Distance(transform.position, pathPoints[i].position);
+            if (dist < minDistance)
+            {
+                minDistance = dist;
+                currentPoint = i;
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+        Gizmos.DrawWireSphere(lastPosition.position, 0.5f);
+    }
+}
